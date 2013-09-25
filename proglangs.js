e4c22f7aaa030d7f7e5eb6c571ee3614f64a431b
emuu <steffanemu@gmail.com>
2013-09-25 13:40:49 +0200

    Endret teksten slik funksjonen blir fungerende.
    Har endret filen slik at outputboxen blir fungerende og skriver ut en tekst når jeg velger ett programmeringsspråk fra dropdown listen i html siden.

--- proglangs.js
+++ proglangs.js
@@ -1,7 +1,5 @@
-var element_unordered_list = document.getElementById("proglangs ");alert(element_unordered_list);
-
 function printToOutputBox(selectobj){
  var e_proglangs = document.getElementById("proglangs");
  var e_outputbox = document.getElementById("outputbox");
- e_outputbox.innerHTML = "Du har selektert programmeringsspråket " + e_proglangs.options[selectobj.selectedIndex].text;
+ e_outputbox.innerHTML = "Du har selektert programmeringsspråket " + e_proglangs.options[selectobj.selectedIndex].text+ "</a>";
 }
